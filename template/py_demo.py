35c31f256746214b66cc1016b2ddb5f77a0c3035
tonylee <tonylee0329@gmail.com>
2015-10-13 11:02:54 +0800

    add auto generate py file.
    add comments

---
+++ template/py_demo.py
@@ -0,0 +1,19 @@
+#!coding=utf-8
+'''
+comment
+'''
+__Author__ = 'Tony'
+
+def get_perimeter(length, width):
+    """
+    function comment 
+    """
+    return length * width
+
+def get_area(length, width):
+    """
+    """
+    return 2*(length+width)
+
+
+if __name__ == '__main__':
