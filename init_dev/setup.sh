05ad45c21f22e4935b7cfb0f0c0727ca72a37044
tonylee <tonylee0329@gmail.com>
2020-10-09 11:10:20 +0800

    update script
    add zshrc template
    
    add file

--- init_dev/setup.sh
+++ init_dev/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# 初始化开发环境脚本
 # setup vi
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
@@ -11,13 +11,13 @@ echo "exec zsh" >> ~/.bash_profile
 # setup pyenv
 PYENV_HOME="${HOME}/.pyenv"
 git clone https://github.com/yyuu/pyenv.git ${PYENV_HOME}
-echo "export PYENV_HOME=\"${PYENV_HOME}\"" | sudo tee -a /etc/profile
-echo 'export PATH="$PYENV_HOME/bin:$PATH"' | sudo tee -a /etc/profile
-echo 'eval "$(pyenv init -)"' | sudo tee -a /etc/profile
+echo "export PYENV_HOME=\"${PYENV_HOME}\"" >> ~/.zshrc
+echo 'export PATH="$PYENV_HOME/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 exec $SHELL
 
 # setup python env
-py2='2.7.10'
+py2='2.7.10' 
 pyenv install $py2
 pyenv global $py2
 ## you need to rehash 
